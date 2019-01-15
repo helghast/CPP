8856d72c9403609e94f1e22b80956749e4ec3c50
helghast <helghast@users.noreply.github.com>
2019-01-15 12:45:43 +0100

    Update main.cpp
    -fix tabs

--- IsNumberPowerOfAnother/main.cpp
+++ IsNumberPowerOfAnother/main.cpp
@@ -11,16 +11,16 @@ using namespace std;
 
 static bool IsPowerV1(const long x, const long y)
 {
-    // logarithm function to calculate value
-    /**! IN CODEBLOCKS THIS OPERATION TO INT DOES NOT WORK WELL (res1 != res2). IN VSTUDIO YES. (res1 != res2).
-    The cast-to-int operation (int)doubleValue performs truncation, which means if the number is represented internally
+	// logarithm function to calculate value
+	/**! IN CODEBLOCKS THIS OPERATION TO INT DOES NOT WORK WELL (res1 != res2). IN VSTUDIO YES. (res1 != res2).
+	The cast-to-int operation (int)doubleValue performs truncation, which means if the number is represented internally
 	as 33.999999999... it will be truncated to 33.
-    Use round() if you need to perform rounding (3.9 → 4, 3.4 → 3). */
-    //const long res1 = log(y) / log(x);
+	Use round() if you need to perform rounding (3.9 → 4, 3.4 → 3). */
+	//const long res1 = log(y) / log(x);
 
 	const long res1 = round(log(y) / log(x));
 	const double res2 = log(y) / log(x); // Note : this is double
-    cout << "IsPowerV1: " << res1 << " : " << res2 << endl;
+	cout << "IsPowerV1: " << res1 << " : " << res2 << endl;
 
 	// compare to the result1 or result2 both are equal
 	return x > 0 && y > 0 && res1 == res2;
@@ -28,35 +28,35 @@ static bool IsPowerV1(const long x, const long y)
 
 static bool IsPowerV2(const int x, const int y)
 {
-    // using less log operations
-    const double power = log(y) / log(x);
-    const double fpower = floor(power);
-    cout << "IsPowerV2: " << power << " : " << fpower << endl;
-    return x > 0 && y > 0 && power == fpower;
+	// using less log operations
+	const double power = log(y) / log(x);
+	const double fpower = floor(power);
+	cout << "IsPowerV2: " << power << " : " << fpower << endl;
+	return x > 0 && y > 0 && power == fpower;
 }
 
 static bool IsPowerV3(const int x, int y)
 {
-    // using a loop
-    if (x == 0 || y == 0) { return false; }
+	// using a loop
+	if (x == 0 || y == 0) { return false; }
 
-    while(y % x == 0){ y /= x; }
-    cout << "IsPowerV3: " << x << " : " << y << endl;
-    return y == 1;
+	while(y % x == 0){ y /= x; }
+	cout << "IsPowerV3: " << x << " : " << y << endl;
+	return y == 1;
 }
 
 int main()
 {
-    const long x = 2; //27
-    const long y = 8; //729
+	const long x = 2; //27
+	const long y = 8; //729
 
-    bool isPower = IsPowerV1(x, y);
-    cout << y << (isPower ? " is power of " : " is not is power of ") << x << endl;
+	bool isPower = IsPowerV1(x, y);
+	cout << y << (isPower ? " is power of " : " is not is power of ") << x << endl;
 
-    isPower = IsPowerV2(x, y);
-    cout << y << (isPower ? " is power of " : " is not is power of ") << x << endl;
+	isPower = IsPowerV2(x, y);
+	cout << y << (isPower ? " is power of " : " is not is power of ") << x << endl;
 
-    isPower = IsPowerV3(x, y);
-    cout << y << (isPower ? " is power of " : " is not is power of ") << x << endl;
-    return 0;
+	isPower = IsPowerV3(x, y);
+	cout << y << (isPower ? " is power of " : " is not is power of ") << x << endl;
+	return 0;
 }
